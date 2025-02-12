a757963ec689ccf5b199eeb0eb7fbabe1dd0eb17
Meaookung144 <kittinanatthasiri@gmail.com>
2025-02-12 20:20:32 +0700

    update UI and ci

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono,IBM_Plex_Sans_Thai } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -12,6 +12,10 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const ibmPlexSansThai = IBM_Plex_Sans_Thai({
+  subsets: ["thai"],
+  weight: ['400', '500', '600', '700'],
+});
 export const metadata: Metadata = {
   title: "Digital Signage",
   description: "For MWIT Digital Signage System",
@@ -25,7 +29,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${ibmPlexSansThai.className} antialiased text-black`}
+        style={{ backgroundImage: 'url(/sky.webp)', backgroundSize: 'cover', backgroundAttachment: 'fixed', backgroundPosition: 'bottom', backgroundRepeat: 'no-repeat', }}
       >
         {children}
       </body>
